137a60e6173cee63f3d588ae3f4ac65ee1dd96c5
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-07-24 12:47:27 -0500

    Add sidebar, fix draft styling

--- replica-client/src/App.jsx
+++ replica-client/src/App.jsx
@@ -1,22 +1,31 @@
-import { Routes, Route, Outlet } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import "./App.css"
 import { Login } from "./components/auth/Login.jsx"
 import { Register } from "./components/auth/Register.jsx"
 import { ApplicationViews } from "./views/ApplicationViews.jsx"
-import {Authorized } from "./views/Authorized.jsx"
+import { Authorized } from "./views/Authorized.jsx"
+import { NavBar } from "./components/nav/NavBar.jsx"
 
 export const App = () => {
- return (
-  <>
-  <Routes>
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} /> 
-  <Route path="*" element={
-    <Authorized>
-      <ApplicationViews />
-    </Authorized>
-  } />
-  </Routes>
-  </>
- )
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="*"
+          element={
+            <Authorized>
+              <div className="main-layout">
+                <NavBar />
+                <main className="main-content">
+                  <ApplicationViews />
+                </main>
+              </div>
+            </Authorized>
+          }
+        />
+      </Routes>
+    </div>
+  )
 }
