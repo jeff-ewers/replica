7e92cff2c7e71438fed275041d4a9ef5ae2720d3
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-07-26 17:04:19 -0500

    Create icons & style sidebar

--- replica-client/src/components/nav/NavBar.jsx
+++ replica-client/src/components/nav/NavBar.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom"
 import logo from '../../assets/replica_logo_tall.png'
+import workflows from '../../assets/workflows.png'
+import models from '../../assets/models.png'
+import projects from '../../assets/projects.png'
+import profile from '../../assets/profile.png'
+import logout from '../../assets/logout.png'
 import "./NavBar.css"
 
 export const NavBar = () => {
@@ -13,19 +18,33 @@ export const NavBar = () => {
         <img src={logo} className='navbar-logo' alt='Replica logo'/>
       </Link>
       <div className="navbar-links">
-        <Link to="/workflows" className='logo-workflow'>Workflows</Link>
-        <Link to="/models" className='logo-models'>Models</Link>
-        <Link to="/projects" className='logo-projects'>Projects</Link>
-        <Link to="/profile" className='logo-profile'>Profile</Link>
+        <Link to="/workflows" className='navbar-item'>
+          <img src={workflows} alt='Workflows' />
+          <span>Workflows</span>
+        </Link>
+        <Link to="/models" className='navbar-item'>
+          <img src={models} alt='Models' />
+          <span>Models</span>
+        </Link>
+        <Link to="/projects" className='navbar-item'>
+          <img src={projects} alt='Projects' />
+          <span>Projects</span>
+        </Link>
+        <Link to="/profile" className='navbar-item'>
+          <img src={profile} alt='Profile' />
+          <span>Profile</span>
+        </Link>
         {localStorage.getItem("replica_user") && (
           <Link
             to=""
+            className='navbar-item'
             onClick={() => {
               localStorage.removeItem("replica_user")
               navigate("/", { replace: true })
             }}
           >
-            Logout
+            <img src={logout} alt='Logout' />
+            <span>Logout</span>
           </Link>
         )}
       </div>
